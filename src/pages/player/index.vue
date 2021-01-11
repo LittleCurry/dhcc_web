4b74a8c887baa6fc728ec1936fd05770d5d735c1
curry <curry@siiva.com>
2021-01-11 13:18:49 +0800

    add sec

--- src/pages/player/index.vue
+++ src/pages/player/index.vue
@@ -17,10 +17,10 @@
             @ready="playerReadied"
           ></video-player>
           <!--<video-->
-            <!--class="video-player-box vjs-custom-skin"-->
-            <!--ref="videoPlayer"-->
-            <!--:options="playerOptions"-->
-            <!--@ready="playerReadied"-->
+          <!--class="video-player-box vjs-custom-skin"-->
+          <!--ref="videoPlayer"-->
+          <!--:options="playerOptions"-->
+          <!--@ready="playerReadied"-->
           <!--&gt;</video>-->
 
 
@@ -33,21 +33,21 @@
                   <p ref="playTitle">{{title}}</p>
                 </div>
                 <!--<div class="play-append">-->
-                  <!--<div class="play-share">-->
-                    <!--<span>分享：</span>-->
-                    <!--<a>-->
-                      <!--<img alt="wechat" src="../../assets/wechat.png">-->
-                    <!--</a>-->
-                    <!--<a>-->
-                      <!--<img alt="sina" src="../../assets/weibo.png">-->
-                    <!--</a>-->
-                  <!--</div>-->
-                  <!--<div class="play-amount">-->
-                    <!--<p>-->
-                      <!--浏览量：-->
-                      <!--<span>{{amount}}</span>-->
-                    <!--</p>-->
-                  <!--</div>-->
+                <!--<div class="play-share">-->
+                <!--<span>分享：</span>-->
+                <!--<a>-->
+                <!--<img alt="wechat" src="../../assets/wechat.png">-->
+                <!--</a>-->
+                <!--<a>-->
+                <!--<img alt="sina" src="../../assets/weibo.png">-->
+                <!--</a>-->
+                <!--</div>-->
+                <!--<div class="play-amount">-->
+                <!--<p>-->
+                <!--浏览量：-->
+                <!--<span>{{amount}}</span>-->
+                <!--</p>-->
+                <!--</div>-->
                 <!--</div>-->
               </div>
             </el-col>
@@ -71,7 +71,7 @@
             <discuss></discuss>
           </el-col>
           <!--<el-col :span="6">-->
-            <!--<recommend :data="rcData" @changeVideoData="changeVideoData"></recommend>-->
+          <!--<recommend :data="rcData" @changeVideoData="changeVideoData"></recommend>-->
           <!--</el-col>-->
         </el-row>
       </div>
@@ -84,114 +84,126 @@
 </template>
 
 <script>
-import "../../assets/css/custom-player.scss";
-import videojs from "video.js";
-window.videojs = videojs;
+  import "../../assets/css/custom-player.scss";
+  import videojs from "video.js";
 
-// hls plugin for videojs6 (replaced by VHS)
-// require("videojs-contrib-hls/dist/videojs-contrib-hls.js");
+  window.videojs = videojs;
 
-export default {
-  components: {},
-  inject: ["reload"],
-  props: {},
-  data() {
-    return {
-      title: "",
-      amount: 0,
-      activeitem: ["", "", "", "", "", "", ""],
-      rcData: this.$store.state.webData.RecommendedVideos.data,
-      videoData: {},
-      isReload: true,
-      playerOptions: {
-        // videojs options
-        autoplay: false,
-        muted: false,
-        loop: false,
-        preload: "auto",
-        language: "en",
-        playbackRates: [0.5, 1.0, 1.5, 2.0],
-        aspectRatio: "16:9",
-        fluid: true,
-        sources: [],
-        flash: { hls: { withCredentials: false } },
-        html5: { hls: { withCredentials: false } },
-        poster: "",
-        notSupportedMessage: "遇到问题，视频无法播放",
-        controlBar: {
-          timeDivider: true,
-          durationDisplay: true,
-          remainingTimeDisplay: false,
-          fullScreenToggle: true
+  // hls plugin for videojs6 (replaced by VHS)
+  // require("videojs-contrib-hls/dist/videojs-contrib-hls.js");
+
+  export default {
+    components: {},
+    inject: ["reload"],
+    props: {},
+    data() {
+      return {
+        title: "",
+        amount: 0,
+        activeitem: ["", "", "", "", "", "", ""],
+        rcData: this.$store.state.webData.RecommendedVideos.data,
+        videoData: {},
+        isReload: true,
+        playerOptions: {
+          // videojs options
+          autoplay: false,
+          muted: false,
+          loop: false,
+          preload: "auto",
+          language: "en",
+          playbackRates: [0.5, 1.0, 1.5, 2.0],
+          aspectRatio: "16:9",
+          fluid: true,
+          // this.playerOptions.sources = this.videoData.source;
+          // sources: [],
+          // src: require(this.videoData.source.src),
+          sources : [ {
+            type : "video/mp4",
+            src : require(this.videoData.source.src)
+          } ],
+          flash: {hls: {withCredentials: false}},
+          html5: {hls: {withCredentials: false}},
+          poster: "",
+          notSupportedMessage: "遇到问题，视频无法播放",
+          controlBar: {
+            timeDivider: true,
+            durationDisplay: true,
+            remainingTimeDisplay: false,
+            fullScreenToggle: true
+          }
         }
+      };
+    },
+    created() {
+    },
+    mounted() {
+      this.videoData = this.$route.query.data;
+      console.log(this.videoData);
+      if (this.videoData == {}) {
+        this.videoData = JSON.parse(localStorage.getItem("shotcut_videoData"));
+      } else {
+        localStorage.setItem("shotcut_videoData", JSON.stringify(this.videoData));
       }
-    };
-  },
-  created() {},
-  mounted() {
-    this.videoData = this.$route.query.data;
-    console.log(this.videoData);
-    if (this.videoData == {}) {
-      this.videoData = JSON.parse(localStorage.getItem("shotcut_videoData"));
-    } else {
-      localStorage.setItem("shotcut_videoData", JSON.stringify(this.videoData));
-    }
-    this.title = this.videoData.title;
-    // if (this.title.length > 12) {
-    //   this.title = this.title.slice(0, 12) + "...";
-    // }
-    this.amount = this.videoData.amount;
-    this.playerOptions.poster = this.videoData.poster;
-    this.playerOptions.sources = this.videoData.source;
-    // this.playerOptions.sources.src = require(this.videoData.source.src);
+      this.title = this.videoData.title;
+      // if (this.title.length > 12) {
+      //   this.title = this.title.slice(0, 12) + "...";
+      // }
+      this.amount = this.videoData.amount;
+      this.playerOptions.poster = this.videoData.poster;
+      this.playerOptions.sources = this.videoData.source;
 
-    //console.log("this is current player instance object", this.player);
-    //console.log(this.rcData);
-  },
-  beforeDestroy() {},
-  destroyed() {},
-  computed: {
-    player() {
-      return this.$refs.videoPlayer.player;
-    }
-  },
-  methods: {
-    playerReadied(player) {
-      if (this.videoData.source.type == "application/x-mpegURL") {
-        let hls = player.tech({ IWillNotUseThisInPlugins: true }).hls;
-        player.tech_.hls.xhr.beforeRequest = function(options) {
-          // console.log(options)
-          return options;
-        };
+
+      // this.playerOptions.sources.src = require(this.videoData.source.src);
+      //console.log("this is current player instance object", this.player);
+      //console.log(this.rcData);
+    },
+    beforeDestroy() {
+    },
+    destroyed() {
+    },
+    computed: {
+      player() {
+        return this.$refs.videoPlayer.player;
       }
     },
-    googleAd() {
-      window.open(
-        "https://www.google.com/adsense/start/#/?modal_active=none",
-        "_blank"
-      );
-    },
-    changeVideoData(recommendData) {
-      this.videoData = recommendData;
-      localStorage.setItem("shotcut_videoData", JSON.stringify(this.videoData));
-      //console.log(this.videoData);
+    methods: {
+      playerReadied(player) {
+        if (this.videoData.source.type == "application/x-mpegURL") {
+          let hls = player.tech({IWillNotUseThisInPlugins: true}).hls;
+          player.tech_.hls.xhr.beforeRequest = function (options) {
+            // console.log(options)
+            return options;
+          };
+        }
+      },
+      googleAd() {
+        window.open(
+          "https://www.google.com/adsense/start/#/?modal_active=none",
+          "_blank"
+        );
+      },
+      changeVideoData(recommendData) {
+        this.videoData = recommendData;
+        localStorage.setItem("shotcut_videoData", JSON.stringify(this.videoData));
+        //console.log(this.videoData);
 
-      this.isReload = false;
+        this.isReload = false;
 
-      this.$nextTick(() => {
-        this.title = this.videoData.title;
-        this.amount = this.videoData.amount;
-        this.playerOptions.poster = this.videoData.poster;
-        this.playerOptions.sources = this.videoData.source;
-        this.isReload = true;
-      });
+        this.$nextTick(() => {
+          this.title = this.videoData.title;
+          this.amount = this.videoData.amount;
+          this.playerOptions.poster = this.videoData.poster;
+          this.playerOptions.sources = this.videoData.source;
+          this.isReload = true;
+        });
+      },
+      changeTitleWidth(e) {
+      }
     },
-    changeTitleWidth(e) {}
-  },
-  watch: {}
-};
+    watch: {}
+  };
 </script>
 
 <style lang="scss" scoped>
-@import "./index";
+  @import "./index";
 </style>
